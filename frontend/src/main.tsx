a721d1834eb03ae42a377cc7846722279021ad5b
Kampott <maximka2030@gmail.com>
2025-05-13 22:49:45 +0300

    Define global color palette and typography

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import App from './App';
 import theme from './theme/theme';
 import './index.css';
-import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
